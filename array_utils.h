f48d0f2f5329a286c1c4f2824af3594a0c0abec0
ihariprasathkannan <hariapk224@gmail.com>
2024-06-12 17:48:10 +0530

    COMMENT = bubble sort files

---
+++ array_utils.h
@@ -0,0 +1,10 @@
+#ifndef ARRAY_UTILS_H
+#define ARRAY_UTILS_H
+
+// Function to print an array
+void printArray(int arr[], int size);
+
+// Function to create an array of specified size with user input
+int* createArray(int size);
+
+#endif
